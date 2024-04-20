8afa0b3abf9b547f898aa017d8c4a0f46db88dae
aluno21530 <aluno21530@ipt.pt>
2024-04-20 22:10:36 +0100

    Product class implementation

--- src/main/java/com/project/ordercartAPI/model/Product.java
+++ src/main/java/com/project/ordercartAPI/model/Product.java
@@ -1,4 +1,23 @@
 package com.project.ordercartAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+
+@Builder
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String unitValue;
+    @Builder.Default
+    private Boolean available = true;
+    @ManyToOne
+    @JsonIgnore
+    private Restaurant restaurant;
 }
