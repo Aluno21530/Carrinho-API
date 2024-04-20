8de496c5adf283843553f901142b1ae8f27422bf
aluno21530 <aluno21530@ipt.pt>
2024-04-20 21:33:28 +0100

    Initial commit

---
+++ src/test/java/com/project/carrinhoApi/CarrinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.carrinhoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
