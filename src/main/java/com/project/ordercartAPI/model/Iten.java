1acf33548596ddc36581403e1ad5eee0f61a7cc1
aluno21530 <aluno21530@ipt.pt>
2024-04-20 22:20:22 +0100

    Iten class implementation

--- src/main/java/com/project/ordercartAPI/model/Iten.java
+++ src/main/java/com/project/ordercartAPI/model/Iten.java
@@ -1,4 +1,17 @@
 package com.project.ordercartAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
 public class Iten {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    private Product product;
+    private int quantity;
+    @ManyToOne
+    @JsonIgnore
+    private OrderCart orderCart;
 }
