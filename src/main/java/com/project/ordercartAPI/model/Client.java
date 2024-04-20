5f30f2946a6dbb42e8ec3e26986f062a55728af8
aluno21530 <aluno21530@ipt.pt>
2024-04-20 22:11:59 +0100

    Client class implementation

--- src/main/java/com/project/ordercartAPI/model/Client.java
+++ src/main/java/com/project/ordercartAPI/model/Client.java
@@ -1,4 +1,16 @@
 package com.project.ordercartAPI.model;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @Embedded
+    private Address address;
 }
