30811408dd529a5ec014c45f36b0982f8dec67fd
aluno21530 <aluno21530@ipt.pt>
2024-04-22 20:05:55 +0100

    Order cart service implementation to add, view and close and order cart

---
+++ src/main/java/com/project/ordercartAPI/service/impl/OrderCartServiceImpl.java
@@ -0,0 +1,75 @@
+package com.project.ordercartAPI.service.impl;
+
+import com.project.ordercartAPI.enumeration.PaymentMethod;
+import com.project.ordercartAPI.model.Iten;
+import com.project.ordercartAPI.model.OrderCart;
+import com.project.ordercartAPI.model.Restaurant;
+import com.project.ordercartAPI.repository.ItenRepository;
+import com.project.ordercartAPI.repository.OrderCartRepository;
+import com.project.ordercartAPI.repository.ProductRepository;
+import com.project.ordercartAPI.resource.dto.ItenDto;
+import com.project.ordercartAPI.service.OrderCartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderCartServiceImpl implements OrderCartService {
+
+    private final OrderCartRepository orderCartRepository;
+    private final ProductRepository productRepository;
+    private final ItenRepository itenRepository;
+
+    @Override
+    public Iten addItenInOrderCart(ItenDto itenDto) {
+        OrderCart orderCart = viewOrderCart(itenDto.getOrderCartId());
+        if (orderCart.isClosed()) {
+            throw new RuntimeException("This order cart is closed");
+        }
+        Iten itenToBeAdd = Iten.builder()
+                .quantity(itenDto.getQuantity())
+                .orderCart(orderCart)
+                .product(productRepository.findById(itenDto.getOrderCartId()).orElseThrow(
+                        () -> {
+                            throw new RuntimeException("This product does not exist");
+                        }
+                ))
+                .build();
+        List<Iten> orderCartItens = orderCart.getItens();
+        if (orderCartItens.isEmpty()) {
+            orderCartItens.add(itenToBeAdd);
+        } else {
+            Restaurant actualRestaurant = orderCartItens.get(0).getProduct().getRestaurant();
+            Restaurant itenToBeAddRestaurant = itenToBeAdd.getProduct().getRestaurant();
+            if (actualRestaurant.equals(itenToBeAddRestaurant)) {
+                orderCartItens.add(itenToBeAdd);
+            } else {
+                throw new RuntimeException("It is not possible to add products from different restaurants. Close the order cart or empty it.");
+            }
+        }
+        orderCartRepository.save(orderCart);
+        return itenRepository.save(itenToBeAdd);
+    }
+
+    @Override
+    public OrderCart viewOrderCart(Long id) {
+        return orderCartRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("This order cart does not exist")
+        );
+    }
+
+    @Override
+    public OrderCart closeOrderCart(Long id, int paymentMethod) {
+        OrderCart orderCart = viewOrderCart(id);
+        if (orderCart.getItens().isEmpty()) {
+            throw new RuntimeException("Empty order cart! Add itens to your order cart");
+        }
+        PaymentMethod paymentMethod1 =
+                paymentMethod == 0 ? PaymentMethod.MONEY : PaymentMethod.CARD;
+        orderCart.setPaymentMethod(paymentMethod1);
+        orderCart.setClosed(true);
+        return orderCartRepository.save(orderCart);
+    }
+}
